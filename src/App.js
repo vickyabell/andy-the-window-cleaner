581e8948b076fcc00dae7be60070c50d18b3beaa
Victoria Weck <victoriaweck2924@gmail.com>
2019-05-27 15:36:02 +0000

    add content, initial styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <header>
+        <h1>Andy the Window Cleaner</h1>
+        <p>Fascias, Soffits, Guttering, Conservatory Roofs</p>
+      </header>
+      <main>
+        <h2>Contact Andrew Morris</h2>
+        <ul>
+          <li>07834 613 151</li>
+          <li>07923 104 961</li>
+          <li>76 Servern Green, Nether Poppleton, York, YO26 6RJ</li>
+        </ul>
+
+        <h2>Pay Online</h2>
+        <p>
+          Account Number: 38790629 <br />
+          Sort Code: 56-00-70
+        </p>
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Typography from 'typography';
+import oceanBeachTheme from 'typography-theme-ocean-beach';
+
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+const typography = new Typography(oceanBeachTheme);
+
+typography.injectStyles();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
